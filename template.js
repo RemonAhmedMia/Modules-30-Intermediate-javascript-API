0e952b13932aa022a470c657e916bbe55341ecf0
TheRemonDev <riyanahmed3536@gmail.com>
2025-09-06 19:39:00 +0100

    template-01

---
+++ template.js
@@ -0,0 +1,4 @@
+const first = 'jaan';
+const last = 'Pakhi';
+const name = first + " " + last;
+console.log(name); // eivhabe kora jai kinto onek somoy sapekkho bepar like amader kache onek gula erokom thakle likhte pera hobe;
